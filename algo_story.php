e530a1d6bb7a9c91633f26c8b5220017380bef55
Andrew Churyumov <34455557+error0001@users.noreply.github.com>
2019-10-29 14:55:46 +0300

    sential linear search

--- algo_story.php
+++ algo_story.php
@@ -11,5 +11,25 @@ function searchI($a, $n, $x){
     return -1;
 }
 
+//$b = [1,2,3,5,7,8];
+//echo searchI($b, count($b), 3);
+
+// sential linear search
+// $a - array, n - count of array, x needed value
+function searchI1($a, $n, $x)
+{
+    $last = $a[$n - 1];
+    $i = 0;
+    while($a[$i] != $x)
+    {
+        $i++;
+    }
+    $a[$n] = $last;
+    if ($i < $n || $a[$n] == $x)
+    {
+        return $i;
+    }
+    return -1;
+}
 $b = [1,2,3,5,7,8];
-echo searchI($b, count($b), 3);
+echo searchI1($b, count($b), 3);
