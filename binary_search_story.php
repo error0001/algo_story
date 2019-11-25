8b43fea7a08bc0d6e3454bcb4c029c98c02c47c9
Andrew Churyumov <34455557+error0001@users.noreply.github.com>
2019-11-25 09:52:02 +0300

    Update binary_search_story.php

--- binary_search_story.php
+++ binary_search_story.php
@@ -1,31 +1,21 @@
 <?php 
 // проблема с диапозоном поиска.
+// уточ. скорее всего уходит в бесконечный луп.
 function bin_srch($a,$n,$x)
 {
     $p = 1;
     $r = $n;
     $q = 0;
-    //r5
     while ($p <= $r)
     {
-        //r5
-        //r4
         $q = floor(($p+$r)/2) + 1;
-        //print_r($a[$q]);
-        //q3    
-        //aq4   3
-        //if ($q > $n || $r > $n)
-        //{return "za predel";}
         if ($a[$q] == $x)
         {
             print_r("\n");
             return $q;
         }
-        //q3
-        //aq4 > 3
         elseif($a[$q] > $x)
         {
-            //r2
             $r = $q - 1;
         }
         elseif($a[$q] < $x)
