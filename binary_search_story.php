21eb1fafd078ecede54995c35a723b1c5e62d32c
Andrew Churyumov <34455557+error0001@users.noreply.github.com>
2019-11-25 12:42:07 +0300

    add function
    add function binary_search_recursive

--- binary_search_story.php
+++ binary_search_story.php
@@ -31,6 +31,31 @@ function binary_srch_basic($a,$n,$x)
     return "Not found";
 }
 
-$s = array(-2,2,3,4,5);
-$out = bin_srch($s, count($s), -2);
-print_r($out);
+//$s = array(-2,2,3,4,5);
+//$out = bin_srch($s, count($s), -2);
+//print_r($out);
+
+function binary_search_recursive($a, $x)
+{
+    $low = 0;
+    $high = count($a);
+    $mid = 0;
+    
+    if($low <= $high)
+    {
+        $mid = floor(($low + $high) / 2);
+        if ($a[$mid] != $x)
+        {
+            if ($a[$mid] > $x)
+            {
+                binary_search_recursive($a, $low,$mid - 1, $x);
+            }
+            else
+            {
+               binary_search_recursive($a, $mid + 1, $high, $x);
+            }
+        }
+        return $mid;
+    }   
+    return "Not found";
+}
